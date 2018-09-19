8e6b5d04cc5cc441cae4fa3cfee1745d03dfec0c
root <root@workcent02.ov.inside.jp>
2018-09-19 18:20:09 +0900

    bug fix

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,21 +1,9 @@
 #!/bin/sh
 
-echo "##########"
-echo "extract dokuwiki.tgz"
-echo "##########"
-
-if [ -f /var/www/html/index.php ] ; then
-  echo "##########"
-  echo "dokuwiki is already extracted"
-  echo "##########"
-else
-  tar xvfz /tmp/dokuwiki.tgz -C /var/www/html --strip-components=1
-  chown -R apache:apache /var/www/html
-  rm -f /tmp/dokuwiki.tgz
-fi
-
 mkdir -p /var/log/httpd
 chown -R apache:apache /var/log/httpd
+
+echo $GREETING > /var/www/html/index.html
 
 echo "##########"
 echo "start apache"
